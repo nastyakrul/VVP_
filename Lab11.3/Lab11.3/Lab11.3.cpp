8e022ef9894f37283966592a2867e4b792c164f4
Анастасия Круглова <nastyakrul@yandex.ru>
2020-11-09 03:55:13 +0300

    11.3

--- Lab11.3/Lab11.3/Lab11.3.cpp
+++ Lab11.3/Lab11.3/Lab11.3.cpp
@@ -2,10 +2,40 @@
 //
 
 #include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian"); //установка русского языка
+    int x1, y1, x2, y2, x3, y3;//ввод переменных
+    double ab, ac;
+    printf("Введите координаты точки A: ");//ввод с клавиатуры значения
+    scanf_s("%d", &x1);
+    scanf_s("%d", &y1);
+    printf("Введите координаты точки B: ");//ввод с клавиатуры значения
+    scanf_s("%d", &x2);
+    scanf_s("%d", &y2);
+    printf("Введите координаты точки C: ");//ввод с клавиатуры значения
+    scanf_s("%d", &x3);
+    scanf_s("%d", &y3);
+    ab = sqrt(pow(abs(x2 - x1), 2) + pow(abs(y2 - y1), 2)); //расстояние между A и B
+    ac = sqrt(pow(abs(x3 - x1), 2) + pow(abs(y3 - y1), 2)); //расстояние между A и C
+    if (ab < ac) //ввод условия
+    {
+        printf("Ближе к точке A расположена точка B с расстоянием = %.2f\n", ab); //вывод на экран
+    }
+    else
+    {
+        if (ab == ac)
+        {
+            printf("Точки находятся на одинаковом расстоянии = %.2f\n", ab);
+        }
+        else
+        printf("Ближе к точке A расположена точка C с расстоянием = %.2f\n", ac);
+    }
+    system("pause");
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
